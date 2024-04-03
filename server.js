0795e3a113aed0a21a9c930b635eedfc8d90c251
afds4567 <33995840+afds4567@users.noreply.github.com>
2024-04-03 22:37:06 +0200

    feat : add Post, Comment Schema

--- server.js
+++ server.js
@@ -7,19 +7,64 @@ const port = 4000;
 
 const schema = buildSchema(`
   type Query{
-    description : String
+    Posts : [Post]
+    Comments : [Comment]
+  }
+
+  type Post{
+    id: ID!
+    title : String!
+    description : String!
+    comments : [Comment]
+  }
+
+  type Comment{
+    id : ID!
+    text : String!
+    likes : Int
   }
 `);
 
 const root = {
-  description: "hello world",
+  Posts: [
+    {
+      id: "post1",
+      title: "first post",
+      description: "first post has own description",
+      comments: [
+        {
+          id: "comment1",
+          text: "It is first comment for post1",
+          likes: 1,
+        },
+        {
+          id: "comment2",
+          text: "It is second comment for post1",
+          likes: 3,
+        },
+      ],
+    },
+  ],
+  Comments: [
+    {
+      id: "comment1",
+      text: "It is first comment for post1",
+      likes: 1,
+    },
+    {
+      id: "comment2",
+      text: "It is second comment for post1",
+      likes: 3,
+    },
+  ],
 };
 
 app.use(
-  "graphql",
+  "/graphql",
   graphqlHTTP({
     schema: schema,
     rootValue: root,
+    graphiql: true,
   })
 );
 
