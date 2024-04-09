699806d761ddc659687bef9b7d5acf8392781cf8
afds4567 <33995840+afds4567@users.noreply.github.com>
2024-04-09 22:41:59 +0200

    refactor : migrate to apollo v3

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-
+const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const app = express();
 const port = 4000;
 
 const loadedTypes = loadFilesSync("**/*", {
@@ -13,23 +11,29 @@ const loadedTypes = loadFilesSync("**/*", {
 
 const loadedResolvers = loadFilesSync(path.join(__dirname, "**/*.resolver.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: loadedTypes,
-  // 스키마의 단일 필드에 대한 데이터를 채우는 역할
-  // ex) 특정 조건에 대한 필터링 적용
-  resolvers: loadedResolvers,
-});
+async function startApolloServer() {
+  const app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: loadedTypes,
+    // 스키마의 단일 필드에 대한 데이터를 채우는 역할
+    // ex) 특정 조건에 대한 필터링 적용
+    resolvers: loadedResolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(port, () => {
+    console.log("Running a GraphQl API server at localhost");
+  });
+}
+
+startApolloServer();
 
 // model내 리턴하는 함수가 없는 경우에만 root필요
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
-
-app.listen(port, () => {
-  console.log("Running a GraphQl API server at localhost");
-});
